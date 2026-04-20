244bc591127427a53664603d7a24623799be6123
Rehan1605 <rm792080@gmail.com>
2026-04-20 08:43:07 +0530

    Added files

---
+++ unidirectional-js/src/components/Parent.jsx
@@ -0,0 +1,12 @@
+import Child from "./Child.jsx";
+const Parent = () => {
+  return (
+    
+        <>
+        <Child key1={"Hello"} key2 = {100}> </Child>
+        
+        </>
+
+    )
+}
+export default Parent;
---
+++ unidirectional-js/src/components/Parent1.jsx
@@ -0,0 +1,12 @@
+import Child1 from "./Child1.jsx";
+const Parent1 = () => {
+  return (
+    
+        <>
+        <Child1 key1={"Hello"} key2 = {100}> </Child1>
+        
+        </>
+
+    )
+}
+export default Parent1;
