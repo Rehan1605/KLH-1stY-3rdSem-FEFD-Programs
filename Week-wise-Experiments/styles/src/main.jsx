78fe3f0ad9f5bda6028862277ab8f0764d575616
Rehan1605 <rm792080@gmail.com>
2026-06-01 18:41:14 +0530

    Styles-React

--- Week-wise-Experiments/styles/src/main.jsx
+++ Week-wise-Experiments/styles/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
+import './style.scss'
+//import App from './App.jsx'
+//import Approach2 from './Approach2.jsx'
+import Approach4 from './Approach4.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/*<App />} */}
+    {/*<Approach4 />} */}
+    <Approach4 />
   </StrictMode>,
 )
